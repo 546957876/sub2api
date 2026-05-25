2e5b864b057dba99355bfe5aea9e510b4d9bba6e
546957876 <54695787@qq.com>
2026-05-25 16:48:46 +0800

    feat: embed pricing page for signed-in users

--- frontend/src/views/public/PricingView.vue
+++ frontend/src/views/public/PricingView.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import PricingView from '@/views/PricingView.vue'
+import AppLayout from '@/components/layout/AppLayout.vue'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+const useEmbeddedLayout = computed(() => authStore.isAuthenticated)
 </script>
 
 <template>
-  <PricingView />
+  <AppLayout v-if="useEmbeddedLayout">
+    <PricingView />
+  </AppLayout>
+  <PricingView v-else />
 </template>
